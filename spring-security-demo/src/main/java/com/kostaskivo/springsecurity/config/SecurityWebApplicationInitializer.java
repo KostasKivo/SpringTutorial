5802a463ec3b398fde70e706214de840a7fde628
Konstantinos Kivotos <k.kivotos@kaizengaming.com>
2022-10-14 14:13:14 +0300

    Spring security + rest service

--- spring-security-demo/src/main/java/com/kostaskivo/springsecurity/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.kostaskivo.springsecurity.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
